63ec9cdb79136ff8d991f81ec233921cebc3b380
viingar <myuniverse8082@gmail.com>
2025-04-08 20:53:45 +0300

    first v.1

--- app/src/main/java/com/example/myapplication/api/LoginResponse.kt
+++ app/src/main/java/com/example/myapplication/api/LoginResponse.kt
@@ -1,7 +1,7 @@
 package com.example.myapplication.api
 
 import com.google.gson.annotations.SerializedName
-
+//
 data class LoginResponse(
     @SerializedName("access_token")
     val accessToken: String,
